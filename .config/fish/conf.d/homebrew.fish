73202adb68c291c18ed8b4abe99487c920e18257
TheAifam5 <theaifam5@gmail.com>
2023-06-09 20:40:30 +0200

    adjust spaces

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -1,12 +1,12 @@
 if [ (arch) = "arm64" ]
-    /opt/homebrew/bin/brew shellenv | source
+  /opt/homebrew/bin/brew shellenv | source
 else
-    /usr/local/bin/brew shellenv | source
+  /usr/local/bin/brew shellenv | source
 end
 
 if test -d "$HOMEBREW_PREFIX/share/fish/completions"
-    set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/completions"
+  set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/completions"
 end
 if test -d "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
-    set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+  set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
 end
