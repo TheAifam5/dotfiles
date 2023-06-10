c89b48fda8b0e73d8e738c37901395868cc79cd5
TheAifam5 <theaifam5@gmail.com>
2023-06-10 19:05:10 +0200

    add homebrew cnf handler

--- .config/fish/conf.d/homebrew.fish
+++ .config/fish/conf.d/homebrew.fish
@@ -7,6 +7,12 @@ end
 if test -d "$HOMEBREW_PREFIX/share/fish/completions"
   set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/completions"
 end
+
 if test -d "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
   set -gx fish_complete_path $fish_complete_path "$HOMEBREW_PREFIX/share/fish/vendor_completions.d"
+end
+
+set HOMEBREW_CNF_HANDLER "$HOMEBREW_REPOSITORY/Library/Taps/homebrew/homebrew-command-not-found/handler.fish"
+if test -f $HOMEBREW_CNF_HANDLER
+  source $HOMEBREW_CNF_HANDLER
 end
